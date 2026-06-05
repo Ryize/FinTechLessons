7399f5839954cedb02611028efb6120f1caab6d3
matvejcekasov <chekashovmatvey@gmail.com>
2026-06-05 14:54:31 +0300

    feat: Добавлены разделение на главные и подключаемые шаблоны

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user_account.views import index
+from user_account.views import index, products
 
 urlpatterns = [
     path('', index),
+    path('products/', products),
 ]
 
 # Главный urls + urls каждой страницы
--- user_account/views.py
+++ user_account/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'user_account/index.html')
+
+
+def products(request):
+    return render(request, 'user_account/products.html')
