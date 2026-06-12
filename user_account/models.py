7ca078f3d604620220ec048892a9a1b7a610706a
matvejcekasov <chekashovmatvey@gmail.com>
2026-06-12 14:18:24 +0300

    feat: Добавлено получение данных feat: Реализована модель Продукта

--- user_account/models.py
+++ user_account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=48, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание товара')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    amount = models.PositiveSmallIntegerField(verbose_name='Количество')
+    created_at = models.DateTimeField(auto_now_add=True)
--- user_account/views.py
+++ user_account/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        print(login, password)
     return render(request, 'user_account/index.html')
 
 
